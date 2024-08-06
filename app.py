bc34a94c8af2a4fab4f5001e5c23356a18b3f210
reconman <reconman@users.noreply.github.com>
2024-08-06 22:24:29 +0200

    Deal with user-attachments links

--- app.py
+++ app.py
@@ -111,7 +111,8 @@ def lambda_handler(event, context):
         comment = str(payload['comment']['body'])
         user = str(payload['comment']['user']['login'])
         match = (re.search(r'http(s)?://[^ >]+?\.(png|jpeg|jpg)', comment)
-                 or re.search(r'https://github\.com/Fate-Grand-Automata/FGA/assets/[-a-f0-9/]+', comment))
+                 or re.search(r'https://github\.com/Fate-Grand-Automata/FGA/assets/[-a-f0-9/]+', comment)
+                 or re.search(r'https://github\.com/user-attachments/assets/[-a-f0-9]+', comment))
         if match:
             logger.info("Found image")
             url = match.group()
